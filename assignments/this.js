09cd115a32a873a263753c4744b547607282c7c2
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2019-10-16 13:01:59 -0700

    answered questions in this module

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Global Binding - if called in the scope of ...
-* 2. Implicit
-* 3. Explicit
-* 4. New
+* 1. Global Binding - if called in global scope, it will reference the global object, which is the browser window or console...
+* 2. Implicit Binding is when 'this' is used in scope of an object and so it references that object
+* 3. New Binding is when 'this' is used to refer to an object that is being created by a object-creator function
+* 4. Explicit Binding is when 'this' in a method that assigns the object this is refering too directly, such as 'call' or 'bind'
 *
 * write out a code example of each explanation above
 */
